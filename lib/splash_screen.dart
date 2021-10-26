c55d58c17f597c6eb8908a6edd5ec6d20b329b1f
Flavio <Flavio>
2021-10-26 15:51:37 +0200

    add colophon

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      Duration(seconds: 3),
+      Duration(seconds: 4),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (BuildContext context) => MovieListings(),
@@ -29,33 +29,55 @@ class _SplashScreenState extends State<SplashScreen> {
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text(
-                'Movie App',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 40.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.white,
-                  thickness: 1,
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 150.0,
+                    ),
+                    Text(
+                      'Movie App',
+                      style: TextStyle(
+                          fontFamily: 'Pacifico',
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                      width: 150.0,
+                      child: Divider(
+                        color: Colors.white,
+                        thickness: 1,
+                      ),
+                    ),
+                    Text(
+                      'View all movie list',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                'View all movie list',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  //color: Colors.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  child: Text(
+                    'developed by Top1 - rev.1.0',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 14.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ],
